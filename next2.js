7dfa0eb41f7d211662e7e45c4f6494d18c2daa8a
Frank <franklynudeorah@yahoo.com>
2022-02-13 00:28:53 +0000

    A little something with swapi

---
+++ next2.js
@@ -0,0 +1,40 @@
+const planetContainer = document.querySelector('.planet-container');
+
+fetch('https://swapi.dev/api/planets/?page=2').then(response =>{
+    return response.json();
+})
+.then(data =>{
+    const planets = data.results;
+    console.log(planets);
+    planets.forEach((el) => 
+    {
+        const planetContent = document.createElement('div');
+        planetContent.classList.add('planet-content');
+        const imgDiv = document.createElement('div');
+        imgDiv.classList.add('img-container');
+        
+        const planet = document.createElement('div');
+        planet.classList.add('planet');
+        const planetName = document.createElement('p');
+        const planetClimate = document.createElement('p');
+        const planetGravity = document.createElement('p');
+        const surfaceWater = document.createElement('p');
+    
+        planetName.innerHTML = 'Name: '+ el.name;
+        planetClimate.innerHTML = 'Climate: '+ el.climate;
+        planetGravity.innerHTML = 'Gravity: ' + el.gravity;
+        surfaceWater.innerHTML = 'Surface water: ' + el.surface_water;
+        imgOutput = `<img src="/images/${el.name}.jpg" alt=""/>`;
+        
+        
+        imgDiv.innerHTML= imgOutput;
+
+        planet.append(planetName, planetClimate, planetGravity, surfaceWater);
+    
+        planetContent.append(imgDiv);
+
+        planetContent.append(planet);
+
+        planetContainer.append(planetContent);
+    }); 
+});
